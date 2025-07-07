ad74c19382d53a937db6149062c73998838676f2
hackgames13 <159486747+hackgames13@users.noreply.github.com>
2025-07-07 14:31:02 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+const WebSocket = require('ws');
+
+const wss = new WebSocket.Server({ port: 8080 });
+
+wss.on('connection', ws => {
+  console.log('Client connected');
+
+  ws.on('message', message => {
+    console.log(`Received: ${message}`);
+    if(message != "ping") {
+        wss.clients.forEach(client => {
+            if (client.readyState === WebSocket.OPEN) {
+                client.send(message.toString());
+            }
+        });
+    }
+  });
+});
